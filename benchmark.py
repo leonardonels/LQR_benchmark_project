c8e39f1e5cefcaf9729bf0b21b7a9b2e1ebcf943
leo <goldenmatrix2.0@gmail.com>
2025-02-04 23:44:57 +0100

    small improvement

--- benchmark.py
+++ benchmark.py
@@ -2,23 +2,36 @@ import subprocess
 import random
 import time
 import pandas as pd
+import numpy as np
 
-######## params ########
+################ params ################
 
 n=10    #number ofexecutions
+trj_min_x=1.0
+trj_max_x=10.0
+trj_min_y=1.0
+trj_max_y=10.0
+odo_min_x=-3.0
+odo_max_x=10.0
+odo_min_y=-5.0
+odo_max_y=5.0
+odo_min_yaw=0.0
+odo_max_yaw=6.28
+plot=True
 
-########################
+########################################
 
+execution_time_n=np.ndarray(n)
 trajectory=pd.read_csv("trajectories/trajectory_downsampled.csv")
-trajectory["x"]*=random.uniform(1.0,10.0)
-trajectory["y"]*=random.uniform(1.0,10.0)
+trajectory["x"]*=random.uniform(trj_min_x,trj_max_x)
+trajectory["y"]*=random.uniform(trj_min_y,trj_max_y)
 trajectory.to_csv("trajectories/test_trajectory_downsampled.csv", index=False)
 try:    
     for i in range(n):
         start_time = time.time()
-        test_odometry=pd.DataFrame({"x":random.uniform(-3.0,10.0),
-                                    "y":random.uniform(-5.0,5.0),
-                                    "yaw":random.uniform(0.0,6.28)
+        test_odometry=pd.DataFrame({"x":random.uniform(odo_min_x,odo_max_x),
+                                    "y":random.uniform(odo_min_y,odo_max_y),
+                                    "yaw":random.uniform(odo_min_yaw,odo_max_yaw)
                                     }, index=[0])
         test_odometry.to_csv("odometry/test_odometry.csv", index=False)
         subprocess.run(["build/knn",
@@ -27,14 +40,17 @@ try:
                         ], check=True)
         end_time = time.time()
         execution_time = end_time - start_time
-        print(f"[BENCHMARK]: {execution_time/100} milliseconds to compute")
+        execution_time_n.put(i,execution_time*1000)
+        print(f"[BENCHMARK]: {execution_time*1000} milliseconds to compute on test {i}")
 
-        subprocess.run(["python3",
-                        "print.py",
-                        "--odometry", "odometry/test_odometry.csv",
-                        "--trajectory", "trajectories/test_trajectory_downsampled.csv",
-                        "--closest_point", "closest_point.csv"
-                        ], check=True)
+        if plot:
+             subprocess.run(["python3",
+                            "print.py",
+                            "--odometry", "odometry/test_odometry.csv",
+                            "--trajectory", "trajectories/test_trajectory_downsampled.csv",
+                            "--closest_point", "closest_point.csv"
+                            ], check=True)
+        print(f"[BENCHMARK]: {execution_time_n.mean()} milliseconds to compute on average")
 
 except KeyboardInterrupt:
         pass
